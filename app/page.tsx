bca30bdea6cb8eebcd37bae9900586b8f6388096
amat3 <121510770+amat3@users.noreply.github.com>
2024-09-04 21:54:12 +0200

    apply styles

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import { EmblaCarousel } from "./components/EmblaCarousel";
 import Footer from "./components/Footer";
 
-const slides = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // Definir el numero de slides
+const slides = [1, 2, 3, 4, 5, 6, 7]; // Definir el numero de slides
 
 export default function Home() {
   return (
